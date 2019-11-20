00df3d1882d2ac1e6e41230b1c5308d9d7ebaa1d
Ubuntu <ubuntu@ip-172-31-25-35.us-west-2.compute.internal>
2019-11-20 23:47:15 +0000

    Added Test Issues

--- index.php
+++ index.php
@@ -4,11 +4,13 @@ $username = 'Cduncan';
 $password = 'Damdog_101';
 $dbname = 'DB';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM users");
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$users = $conn->query("SELECT * FROM users");
+$userslist = $users->fetchAll(PDO::FETCH_ASSOC);
+$issues = $conn->query("SELECT * FROM issues");
+$issueslist = $issues->fetchAll(PDO::FETCH_ASSOC);
 
- if(isset($_REQUEST['display_id'])){
-    foreach ($results as $user):
+ if(isset($_REQUEST['display_users'])){
+    foreach ($userslist as $user):
         $line=$user['id'].','.$user['lastname'] .','.$user['firstname'];
         echo $line;
         echo "<br>";
@@ -17,6 +19,14 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	}
 	
+ if(isset($_REQUEST['display_issues'])){
+    foreach ($issueslist as $issue):
+        $line=$issue['id'].','.$issue['title'] .','.$issue['priority'];
+        echo $line;
+        echo "<br>";
+        
+    endforeach;
 
+	}
 
 ?>
